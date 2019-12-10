0f7a3b859d7ddb3d483d3d5897dea90a2f35d841
raythurman2386 <raymondthurman5@gmail.com>
2019-12-09 19:06:17 -0500

    logger middleware

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
-
 //custom middleware
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
 
-function logger(req, res, next) {}
+  next();
+}
+
+server.use(logger);
 
 module.exports = server;
