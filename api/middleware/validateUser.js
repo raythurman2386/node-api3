855ed54e04507bdd6a055a8680e888fe08258712
raythurman2386 <raymondthurman5@gmail.com>
2019-12-10 20:19:03 -0500

    validateUser middleware

--- api/middleware/validateUser.js
+++ api/middleware/validateUser.js
@@ -1,3 +1,19 @@
-function validateUser(req, res, next) {
-  // do your magic!
-}
+const db = require("../routes/users/userDb");
+
+const validateUser = () => {
+  return (req, res, next) => {
+    // do your magic!
+    if (!req.body.name) {
+      res.status(404).json({ message: "Please supply a name" });
+    }
+
+    db.insert(req.body)
+      .then(user => {
+        req.newUser = user;
+        next();
+      })
+      .catch(err => next(err));
+  };
+};
+
+module.exports = validateUser;
