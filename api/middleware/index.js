650c450368542742a0fe5b5888d7387bf74afe19
raythurman2386 <raymondthurman5@gmail.com>
2019-12-10 20:19:17 -0500

    implemented custom middleware

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,12 +1,9 @@
 const helmet = require("helmet");
 const cors = require("cors");
 const logger = require("./logger");
-const { routeError, serverError } = require("./errors");
 
 module.exports = server => {
   server.use(helmet());
   server.use(cors());
   server.use(logger());
-  server.use(routeError());
-  server.use(serverError());
 };
--- api/routes/users/userRouter.js
+++ api/routes/users/userRouter.js
@@ -1,35 +1,66 @@
+// @ts-nocheck
 const express = require("express");
+const db = require("./userDb");
 const validateUserId = require("../../middleware/validateUserId");
+const validateUser = require("../../middleware/validateUser");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", validateUser(), (req, res) => {
   // do your magic!
+  res.json(req.body);
 });
 
 router.post("/:id/posts", validateUserId(), (req, res) => {
+  // TODO
   // do your magic!
+  if (!req.body.text) {
+    res.status(404).json({ message: "Please supply a name" });
+  }
 });
 
 router.get("/", (req, res) => {
   // do your magic!
-  res.status(200).json({ message: "User Works" });
+  db.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => next(err));
 });
 
 router.get("/:id", validateUserId(), (req, res) => {
   // do your magic!
+  res.json(req.user);
 });
 
 router.get("/:id/posts", validateUserId(), (req, res) => {
   // do your magic!
+  db.getUserPosts(req.user.id)
+    .then(userPosts => {
+      res.status(200).json(userPosts);
+    })
+    .catch(err => next(err));
 });
 
 router.delete("/:id", validateUserId(), (req, res) => {
   // do your magic!
+  db.remove(req.user.id)
+    .then(user => {
+      res.status(200).json(req.user);
+    })
+    .catch(err => next(err));
 });
 
 router.put("/:id", validateUserId(), (req, res) => {
+  if (!req.body.name) {
+    res.status(404).json({ message: "Please supply a name" });
+  }
   // do your magic!
+  db.update(req.user.id, req.body)
+    .then(user => {
+      res.status(200).json(req.body);
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
