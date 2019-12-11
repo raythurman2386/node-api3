df02b6385d2737c969d7fb9ea621547f134bceb4
raythurman2386 <raymondthurman5@gmail.com>
2019-12-10 20:31:12 -0500

    validatePost middleware

--- api/middleware/validatePost.js
+++ api/middleware/validatePost.js
@@ -1,3 +1,23 @@
-function validatePost(req, res, next) {
-  // do your magic!
-}
+const db = require("../routes/posts/postDb");
+
+const validatePost = () => {
+  return (req, res, next) => {
+    // do your magic!
+    if (!req.body) {
+      res.status(404).json({ message: "Please supply a post" });
+    }
+  };
+};
+
+const validatePostId = () => {
+  return (req, res, next) => {
+    db.getById(req.params.id)
+      .then(postId => {
+        req.postId = postId;
+        next();
+      })
+      .catch(err => next(err));
+  };
+};
+
+module.exports = { validatePost, validatePostId };
