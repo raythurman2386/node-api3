9ab1b6b652abeac56e3e2b620637066b22baa439
raythurman2386 <raymondthurman5@gmail.com>
2019-12-10 20:18:47 -0500

    updated routes, added error routes

--- api/routes/test/test.routes.js
+++ api/routes/test/test.routes.js
@@ -6,4 +6,8 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "Did this work??" });
 });
 
+router.get("/api", (req, res) => {
+  res.status(200).json({ message: "Welcome to Node 3 API" });
+});
+
 module.exports = router;
