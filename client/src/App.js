17b5c00b80f716f6089dbdfb3e4aaed977e4af94
raythurman2386 <raymondthurman5@gmail.com>
2019-12-13 17:07:40 -0500

    added form for image uploader

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,12 @@ import { Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>;
+      <h1>Hello World</h1>;<h1>Upload Image</h1>
+      {/* Skyelars image upload challenge */}
+      <form action="/upload" method="post" enctype="multipart/form-data">
+        <input type="file" accept="image/*" name="photo" />
+        <input type="submit" value="upload" />
+      </form>
     </div>
   );
 }
