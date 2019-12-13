6c406d68cd31b92c49ba394f8e593f5fc2508c7e
raythurman2386 <raymondthurman5@gmail.com>
2019-12-13 16:35:08 -0500

    index boilerplate in place

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { BrowserRouter as Router, withRouter } from "react-router-dom";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import App from "./App";
 
-import App from './App';
+const AppWithRouter = withRouter(App);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
+render(
+  <Provider store={store}>
+    <Router>
+      <AppWithRouter />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
