7aa284705d7b612ed4f2db95eaa118016d03cc53
raythurman2386 <raymondthurman5@gmail.com>
2019-12-13 16:49:15 -0500

    post actions files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Route, Link } from "react-router-dom";
 
 function App() {
-    return (
-        <h1>Hello World</h1>
-    )
+  return (
+    <div className="App">
+      <h1>Hello World</h1>;
+    </div>
+  );
 }
 
 export default App;
