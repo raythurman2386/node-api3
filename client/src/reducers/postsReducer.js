eae900de930c680f06466e4d35d1581f39a7d8d9
raythurman2386 <raymondthurman5@gmail.com>
2019-12-13 16:34:55 -0500

    initial reducers in place

---
+++ client/src/reducers/postsReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isFetching: false,
+  postsData: [],
+  errors: null
+};
+
+export const postReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
---
+++ client/src/reducers/usersReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isFetching: false,
+  usersData: [],
+  errors: null
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
