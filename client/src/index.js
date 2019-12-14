876a8ac125f70f32cd312a35c844debb36f24a7b
raythurman2386 <raymondthurman5@gmail.com>
2019-12-14 14:22:46 -0500

    commented out reduxdevtools

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,15 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { BrowserRouter as Router, withRouter } from "react-router-dom";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 import App from "./App";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const AppWithRouter = withRouter(App);
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk, logger))
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 render(
   <Provider store={store}>
