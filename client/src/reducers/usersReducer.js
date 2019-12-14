9da9cc2b35f03c54ff606677207ab0ecd7626d0e
raythurman2386 <raymondthurman5@gmail.com>
2019-12-14 15:46:58 -0500

    users reducer set up, may need bug fixes once implemented

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -14,7 +14,7 @@ import {
 } from "../actions/userActions";
 
 const initialState = {
-  isFetching: false,
+  isLoading: false,
   usersData: [],
   errors: null
 };
@@ -22,29 +22,94 @@ const initialState = {
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_USERS_START:
-      return state;
+      return {
+        ...state,
+        isLoading: true,
+        usersData: [],
+        errors: ""
+      };
     case FETCH_USERS_SUCCESS:
-      return state;
+      return {
+        ...state,
+        isLoading: false,
+        usersData: action.payload,
+        errors: ""
+      };
     case FETCH_USERS_ERROR:
-      return state;
+      return {
+        ...state,
+        isLoading: false,
+        usersData: [],
+        errors: action.payload
+      };
     case ADD_USER_START:
-      return state;
+      return {
+        ...state,
+        isLoading: true,
+        usersData: [],
+        errors: ""
+      };
     case ADD_USER_SUCCESS:
-      return state;
+      return {
+        ...state,
+        isLoading: false,
+        usersData: [...state.usersData, action.payload],
+        errors: ""
+      };
     case ADD_USER_ERROR:
-      return state;
+      return {
+        ...state,
+        isLoading: false,
+        usersData: [],
+        errors: action.payload
+      };
     case UPDATE_USER_START:
-      return state;
+      return {
+        ...state,
+        isLoading: true,
+        usersData: [],
+        errors: ""
+      };
     case UPDATE_USER_SUCCESS:
-      return state;
+      return {
+        ...state,
+        isLoading: false,
+        usersData: [
+          state.usersData.filter(user => user.id !== action.payload.id),
+          action.payload
+        ],
+        errors: ""
+      };
     case UPDATE_USER_ERROR:
-      return state;
+      return {
+        ...state,
+        isLoading: false,
+        usersData: [],
+        errors: action.payload
+      };
     case DELETE_USER_START:
-      return state;
+      return {
+        ...state,
+        isLoading: true,
+        usersData: [],
+        errors: ""
+      };
     case DELETE_USER_SUCCESS:
-      return state;
+      return {
+        ...state,
+        isLoading: false,
+        usersData: state.usersData.filter(
+          user => user.id !== action.payload.id
+        ),
+        errors: ""
+      };
     case DELETE_USER_ERROR:
-      return state;
+      return {
+        ...state,
+        isLoading: false,
+        usersData: [],
+        errors: action.payload
+      };
     default:
       return state;
   }
