6280556fce780e408a818313bcb05f08ee76b361
raythurman2386 <raymondthurman5@gmail.com>
2019-12-21 20:09:37 -0600

    userRouter fully refactored

--- api/routes/users/userRouter.js
+++ api/routes/users/userRouter.js
@@ -50,46 +50,42 @@ const addUser = async (req, res, next) => {
 };
 
 // add a user post
+const addUserPost = async (req, res, next) => {
+  try {
+    const newPost = posts.insert({ ...req.body, user_id: req.params.id });
+    return res.status(200).json(newPost);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // Update a user
+const updateUser = async (req, res, next) => {
+  try {
+    const user = await db.update(req.user.id, req.body);
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // delete a user
+const deleteUser = async (req, res, next) => {
+  try {
+    const user = await db.remove(req.user.id);
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
 
 userRouter
-
   .post("/", validateUser(), addUser)
-
-  .post("/:id/posts", validateUserId(), validatePost(), (req, res, next) => {
-    posts
-      .insert({ ...req.body, user_id: req.params.id })
-      .then(post => {
-        res.status(201).json(post);
-      })
-      .catch(err => next(err));
-  })
-
+  .post("/:id/posts", validateUserId(), validatePost(), addUserPost)
   .get("/", getUsers)
-
   .get("/:id", validateUserId(), getUserById)
-
   .get("/:id/posts", validateUserId(), getUsersPost)
-
-  .delete("/:id", validateUserId(), (req, res) => {
-    // do your magic!
-    db.remove(req.user.id)
-      .then(user => {
-        res.status(200).json(req.user);
-      })
-      .catch(err => next(err));
-  })
-
-  .put("/:id", validateUserId(), validateUser(), (req, res) => {
-    // do your magic!
-    db.update(req.user.id, req.body)
-      .then(user => {
-        res.status(200).json(req.body);
-      })
-      .catch(err => next(err));
-  });
+  .delete("/:id", validateUserId(), deleteUser)
+  .put("/:id", validateUserId(), validateUser(), updateUser);
 
 module.exports = userRouter;
