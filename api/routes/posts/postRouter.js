99b47e4b1a245db71bd6f063fbb25708a74ab5dd
raythurman2386 <raymondthurman5@gmail.com>
2019-12-21 20:20:06 -0600

    postRouter refactored

--- api/routes/posts/postRouter.js
+++ api/routes/posts/postRouter.js
@@ -7,29 +7,54 @@ const {
   validatePostId
 } = require("../../middleware/validatePost");
 
+// get posts
+const getPosts = async (req, res, next) => {
+  try {
+    const posts = await db.get();
+    return res.status(200).json(posts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// get post by id
+const getPostById = async (req, res, next) => {
+  try {
+    const post = await db.get(req.params.id);
+    return res.status(200).json(post);
+  } catch (error) {
+    next(error);
+  }
+};
+// update post
+const updatePost = async (req, res, next) => {
+  const updatedPost = {
+    user_id: req.body.user_id,
+    text: req.body.text
+  };
+
+  try {
+    const post = await db.update(req.params.id, updatedPost);
+    return res.status(200).json(post);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// delete post
+const deletePost = async (req, res, next) => {
+  try {
+    const post = await db.remove(req.post.id);
+    return res.status(200).json(post);
+  } catch (error) {
+    next(error);
+  }
+};
+
 postRouter
-  .get("/", (req, res, next) => {
-    // do your magic!
-    db.get()
-      .then(posts => {
-        res.status(200).json(posts);
-      })
-      .catch(err => next(err));
-  })
-  .get("/:id", validatePostId(), (req, res) => {
-    // do your magic!
-    res.status(200).json(req.post);
-  })
-  .delete("/:id", validatePostId(), (req, res, next) => {
-    // do your magic!
-    db.remove(req.post.id)
-      .then(post => {
-        res.json(post);
-      })
-      .catch(err => next());
-  })
-  .put("/:id", validatePostId(), validatePost(), (req, res) => {
-    // do your magic!
-  });
+  .get("/", getPosts)
+  .get("/:id", validatePostId(), getPostById)
+  .delete("/:id", validatePostId(), deletePost)
+  .put("/:id", validatePostId(), validatePost(), updatePost);
 
 module.exports = postRouter;
